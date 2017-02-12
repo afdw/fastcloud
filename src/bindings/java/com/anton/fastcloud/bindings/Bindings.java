83849c42211e0e216171a296f0d3c629059e72fa
Anton Danilkin <afdw@yandex.ru>
2017-02-12 16:54:02 +0300

    Extract constant

--- src/bindings/java/com/anton/fastcloud/bindings/Bindings.java
+++ src/bindings/java/com/anton/fastcloud/bindings/Bindings.java
@@ -5,10 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Bindings {
+    public static final String[] LIB_NAMES = {"libbindings.so"};
+
     public static void init() {
         try {
             Path tempDirPath = Files.createTempDirectory("fastcloud-natives");
-            for (String libName : new String[] {"libbindings.so"}) {
+            for (String libName : LIB_NAMES) {
                 Path libPath = tempDirPath.resolve(libName);
                 Files.copy(FuseLibNative.class.getResourceAsStream("/" + libName), libPath);
                 System.load(libPath.toString());
