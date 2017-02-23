8128441a874c839b89561d64a076b0ff1d05a2e4
Anton Danilkin <afdw@yandex.ru>
2017-02-23 18:16:37 +0300

    Prevent Natives double init

--- src/natives/java/com/anton/fastcloud/natives/Natives.java
+++ src/natives/java/com/anton/fastcloud/natives/Natives.java
@@ -5,9 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Natives {
-    public static final String[] LIB_NAMES = {"libnatives.so"};
+    private static boolean inited = false;
+    private static final String[] LIB_NAMES = {"libnatives.so"};
 
     public static void init() {
+        if (inited) {
+            return;
+        }
         try {
             Path tempDirPath = Files.createTempDirectory("fastcloud-natives");
             for (String libName : LIB_NAMES) {
@@ -21,6 +25,7 @@ public class Natives {
             if (!tempDirPath.toFile().delete()) {
                 throw new IOException("Can't remove temp files");
             }
+            inited = true;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
